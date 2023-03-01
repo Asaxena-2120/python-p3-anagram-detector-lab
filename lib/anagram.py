7156dceb122749df2ce76436f102381690eca205
Aastha <saxena.aastha91@gmail.com>
2023-03-01 22:44:19 +0530

    checked anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+    def match(self,match_word_list):
+        result=[]
+        for word in match_word_list:
+            match_word_list=[letter for letter in word]
+            if sorted(match_word_list)==sorted(self.word):
+                result.append(word)
+        return result
